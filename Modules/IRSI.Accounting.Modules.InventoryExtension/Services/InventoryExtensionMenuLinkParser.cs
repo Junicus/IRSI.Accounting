37fe4b2516b0c057515025f0dd5e82f8be85d847
Nelson Segarra <junicus@outlook.com>
2017-06-30 17:14:18 -0400

    refactored class to make functions smaller

--- Modules/IRSI.Accounting.Modules.InventoryExtension/Services/InventoryExtensionMenuLinkParser.cs
+++ Modules/IRSI.Accounting.Modules.InventoryExtension/Services/InventoryExtensionMenuLinkParser.cs
@@ -14,11 +14,11 @@ namespace IRSI.Accounting.Modules.InventoryExtension.Services
 {
   public class InventoryExtensionMenuLinkParser : IInventoryExtensionParser
   {
-	private IEnumerable<Store> _stores = null;
 	private static readonly Logger log = LogManager.GetCurrentClassLogger();
-	private readonly IInventoryExtensionLineParser _lineParser;
 	private readonly IInventoryExtensionFileReader _fileReader;
+	private readonly IInventoryExtensionLineParser _lineParser;
 	private readonly IStoresRepository _storeRepository;
+	private IEnumerable<Store> _stores = null;
 
 	public InventoryExtensionMenuLinkParser(IStoresRepository storeRepository, IInventoryExtensionLineParser lineParser, IInventoryExtensionFileReader fileReader)
 	{
@@ -30,29 +30,46 @@ namespace IRSI.Accounting.Modules.InventoryExtension.Services
 	public IEnumerable<InventoryExtensionItem> ParseFile(string filename)
 	{
 	  var lines = _fileReader.ReadFile(filename);
-	  var storeNumber = string.Empty;
-	  if (lines.Any())
-		storeNumber = lines.First().Split(' ')[0];
-	  if (_stores == null)
-	  {
-		_stores = _storeRepository.GetStores();
-	  }
-	  var store = _stores.SingleOrDefault(s => s.Number == storeNumber);
-	  if (store == null)
-	  {
-		throw new Exceptions.InvalidDataException();
-	  }
+	  var store = GetStore(lines);
+	  return GetItems(lines, store);
+	}
 
-	  List<InventoryExtensionItem> items = new List<InventoryExtensionItem>();
+	private List<InventoryExtensionItem> GetItems(IEnumerable<string> lines, Store store)
+	{
+	  var items = new List<InventoryExtensionItem>();
 	  foreach (var line in lines)
 	  {
 		var item = _lineParser.ParseLine(store, line);
 		if (item != null)
-		{
 		  items.Add(item);
-		}
 	  }
 	  return items;
+	}
+
+	private Store GetStore(IEnumerable<string> lines)
+	{
+	  string storeNumber = GetStoreNumber(lines);
+	  LoadStores();
+	  var store = _stores.SingleOrDefault(s => s.Number == storeNumber);
+	  if (store == null)
+		throw new Exceptions.InvalidDataException();
+	  return store;
+	}
+
+	private void LoadStores()
+	{
+	  if (_stores == null)
+	  {
+		_stores = _storeRepository.GetStores();
+	  }
+	}
+
+	private static string GetStoreNumber(IEnumerable<string> lines)
+	{
+	  var storeNumber = string.Empty;
+	  if (lines.Any())
+		storeNumber = lines.First().Split(' ')[0];
+	  return storeNumber;
 	}
   }
 }
