92036c6412d86f77765c70f567091a37618fc879
Nelson Segarra <junicus@outlook.com>
2017-06-30 18:43:32 -0400

    wrote more tests for viewmodles

---
+++ Tests/IRSI.Accounting.Models.InventoryExtension.Tests/AmountSumConverterTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using IRSI.Accounting.Modules.InventoryExtension.Models;
+using IRSI.Accounting.Modules.InventoryExtension.Views.Converters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IRSI.Accounting.Models.InventoryExtension.Tests
+{
+  [TestClass]
+  public class AmountSumConverterTests
+  {
+	InventoryExtensionItem item1 = new InventoryExtensionItem { Amount = 100.0m, Tax = 100.0m };
+	InventoryExtensionItem item2 = new InventoryExtensionItem { Amount = 200.0m, Tax = 200.0m };
+	InventoryExtensionItem item3 = new InventoryExtensionItem { Amount = 100.0m, Tax = 0m };
+	InventoryExtensionItem item4 = new InventoryExtensionItem { Amount = 200.0m, Tax = 0m };
+	InventoryExtensionItem item5 = new InventoryExtensionItem { Amount = 0m, Tax = 100m };
+	InventoryExtensionItem item6 = new InventoryExtensionItem { Amount = 0m, Tax = 200m };
+
+
+	private readonly AmountSumConverter _subject = new AmountSumConverter();
+
+	[TestMethod]
+	[ExpectedException(typeof(NotImplementedException))]
+	public void ConvertBachShouldThrowNotImplementedException()
+	{
+	  _subject.ConvertBack(null, default(Type), null, CultureInfo.InvariantCulture);
+	}
+
+	[TestMethod]
+	public void ConvertShouldReturn0IfNotCorrectType()
+	{
+	  var result = _subject.Convert(null, default(Type), null, CultureInfo.InvariantCulture);
+	  Assert.AreEqual(0, result);
+	}
+
+	[TestMethod]
+	public void ConvertShouldReturnSumOfItems()
+	{
+	  var result = _subject.Convert(new List<InventoryExtensionItem> { item1, item2 }, default(Type), null, CultureInfo.InvariantCulture);
+	  Assert.AreEqual(600m, result);
+	}
+
+	[TestMethod]
+	public void ConvertShouldReturnSumOfItemsMixed()
+	{
+	  var result = _subject.Convert(new List<InventoryExtensionItem> { item3, item5 }, default(Type), null, CultureInfo.InvariantCulture);
+	  Assert.AreEqual(200m, result);
+	}
+
+	[TestMethod]
+	public void ConvertShouldReturnSumOfItemsOnlyAmount()
+	{
+	  var result = _subject.Convert(new List<InventoryExtensionItem> { item3, item4 }, default(Type), null, CultureInfo.InvariantCulture);
+	  Assert.AreEqual(300m, result);
+	}
+
+	[TestMethod]
+	public void ConvertShouldReturnSumOfItemsOnlyTax()
+	{
+	  var result = _subject.Convert(new List<InventoryExtensionItem> { item5, item6 }, default(Type), null, CultureInfo.InvariantCulture);
+	  Assert.AreEqual(300m, result);
+	}
+  }
+}
---
+++ Tests/IRSI.Accounting.Models.InventoryExtension.Tests/ViewModels/InventoryExtensionModuleViewModelTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using IRSI.Accounting.Common.Events;
+using IRSI.Accounting.Modules.InventoryExtension.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Prism.Events;
+using Prism.Regions;
+
+namespace IRSI.Accounting.Models.InventoryExtension.Tests.ViewModels
+{
+  [TestClass]
+  public class InventoryExtensionModuleViewModelTests
+  {
+	private readonly Mock<NavigationCompletedEvent> _event = new Mock<NavigationCompletedEvent>();
+	private readonly Mock<IRegionManager> _regionManagerMock = new Mock<IRegionManager>();
+	private readonly Mock<IEventAggregator> _eventAggregatorMock = new Mock<IEventAggregator>();
+
+	private readonly InventoryExtensionModuleViewModel _subject;
+
+	public InventoryExtensionModuleViewModelTests()
+	{
+	  //_event.Setup(x => x.Subscribe(It.IsAny<Action<string>>(), ThreadOption.UIThread));
+	  _eventAggregatorMock.Setup(x => x.GetEvent<NavigationCompletedEvent>()).Returns(_event.Object);
+	  _subject = new InventoryExtensionModuleViewModel(_regionManagerMock.Object, _eventAggregatorMock.Object);
+	}
+
+	[TestMethod]
+	public void NewInventoryExtensionGetsEvent()
+	{
+	  Mock<NavigationCompletedEvent> navevent = new Mock<NavigationCompletedEvent>();
+	  Mock<IRegionManager> regionManagerMock = new Mock<IRegionManager>();
+	  Mock<IEventAggregator> eventAggregatorMock = new Mock<IEventAggregator>();
+
+	  //navevent.Setup(x => x.Subscribe(It.IsAny<Action<string>>(), ThreadOption.UIThread));
+	  eventAggregatorMock.Setup(x => x.GetEvent<NavigationCompletedEvent>()).Returns(navevent.Object);
+	  InventoryExtensionModuleViewModel subject = new InventoryExtensionModuleViewModel(regionManagerMock.Object, eventAggregatorMock.Object);
+
+	  eventAggregatorMock.Verify(x => x.GetEvent<NavigationCompletedEvent>());
+	}
+
+	[TestMethod]
+	public void NewInventoryExtensionHasCommand()
+	{
+	  Assert.IsNotNull(_subject.ShowInventoryExtensionView);
+	}
+
+	[TestMethod]
+	public void NewInventoryIsNavigationTargetIsFalse()
+	{
+	  Assert.IsFalse(_subject.IsNavigationTarget(null));
+	}
+
+	[TestMethod]
+	public void ExecutingShowInventoryCallsRequestNavigateOnRegionManager()
+	{
+	  _subject.ShowInventoryExtensionView.Execute(null);
+	  _regionManagerMock.Verify(x => x.RequestNavigate(It.IsAny<string>(), It.IsAny<Uri>(), It.IsAny<Action<NavigationResult>>()));
+	}
+  }
+}
